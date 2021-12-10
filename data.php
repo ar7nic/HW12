b5e3f0540ccbb6fbf56768b5e8944d2d2d28307c
Alex <ar7nic@gmail.com>
2021-12-10 10:23:52 +0200

    layout-php

---
+++ data.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    'mainMenu' => [
+        [
+            'title' => 'Home',
+            'url' => '#',
+        ],
+        [
+            'title' => 'About',
+            'url' => '#',
+        ],
+        [
+            'title' => 'Work',
+            'url' => '#',
+        ],
+        [
+            'title' => 'Services',
+            'url' => '#',
+        ],
+        [
+            'title' => 'Blog',
+            'url' => '#',
+        ],
+    ],
+    'siteLogo' => 'img/some.png',
+    'mainSlider' => [
+        [
+            'title' => [
+                'main' => 'Alecto',
+                'second' => 'is simple PSD Template',
+            ],
+            'description' => 'Nam varius accumsan elementum aliquam',
+            'link' => [
+                'text' => 'Learn more',
+                'url' => '#',
+            ],
+        ],
+        [
+            'title' => [
+                'main' => 'Alecto',
+                'second' => 'is simple PSD Template',
+            ],
+            'description' => 'Nam varius accumsan elementum aliquam',
+            'link' => [
+                'text' => 'Learn more',
+                'url' => '#',
+            ],
+        ],
+    ],
+];
